4a80106e2a2687b7847170d0795a7f4c2efc2f58
n_zhuchkevich <nikitazhuchkevich@gmail.com>
2018-11-26 23:03:18 +0200

    Adding select to main activity

--- Mobile/app/src/main/java/com/example/nikita/mymoney/database/manager/CashManager.kt
+++ Mobile/app/src/main/java/com/example/nikita/mymoney/database/manager/CashManager.kt
@@ -2,27 +2,41 @@ package com.example.nikita.mymoney.database.manager
 
 import android.content.Context
 import com.example.nikita.mymoney.database.model.Cash
+import com.example.nikita.mymoney.database.model.CashCategoryJoinTable
 import com.example.nikita.mymoney.database.model.CashDTO
 import com.example.nikita.mymoney.database.model.Category
-import org.jetbrains.anko.db.*
+import org.jetbrains.anko.db.classParser
+import org.jetbrains.anko.db.parseList
+import org.jetbrains.anko.db.select
 
 class CashManager(_ctx: Context) : SimpleManager(_ctx) {
 
-    //Category.id,  Cash.name, Category.name, cost, date
     fun getCash(): List<CashDTO> {
         return database.use {
-            select(Cash.TABLE_NAME + " inner join " + Category.TABLE_NAME + " on Cash.categoryId = Category.id", "Category.id, Cash.name, Category.name, cost, date").exec {
-                parseList(object : MapRowParser<CashDTO> {
-                    override fun parseRow(columns: Map<String, Any?>): CashDTO {
-                        return CashDTO(id = columns["Cash.id"] as Int, name = columns["Cash.name"] as String,
-                                category = Category(columns["Category.id"] as Int, columns["Category.name"] as String)
-                                , cost = columns["cost"] as Double, date = columns["date"] as String)
+            select(Cash.TABLE_NAME + " left join " + Category.TABLE_NAME,
+                    "Cash.id as cashId, Category.id as categoryId, Cash.name as cashName, Category.name as categoryName, cost, date")
+                    .whereArgs("Cash.categoryId = Category.id")
+                    .exec {
+                        parseList<CashCategoryJoinTable>(classParser())
                     }
-                })
-
-            }
+        }.map {
+            CashDTO(id = it.cashId,
+                    name = it.cashName,
+                    category = Category(it.categoryId, it.categoryName),
+                    cost = it.cost,
+                    date = it.date)
         }
     }
+
+/*
+    db.select("User", "name")
+    .whereArgs("(_id > {userId}) and (name = {userName})",
+    "userName" to "John",
+    "userId" to 42)
+*/
+
+    /*data class CashDTO(var id: Long? = null, val category: Category, var name: String,
+                       val cost: Double, val date: String = LocalDate.now().toString()) {*/
 
     fun getAllCategories(): List<Category> {
         return database.use {
